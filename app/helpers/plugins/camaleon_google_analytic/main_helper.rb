614ed697e0c4e30979f407cd7714c10c25e4300c
Huy Nguyen <huyngvu@gmail.com>
2016-09-25 08:44:32 +0700

    fix load analytics code to frontend

--- app/helpers/plugins/camaleon_google_analytic/main_helper.rb
+++ app/helpers/plugins/camaleon_google_analytic/main_helper.rb
@@ -27,8 +27,16 @@ module Plugins::CamaleonGoogleAnalytic::MainHelper
     camaleon_google_analytics_meta = current_site.get_meta('camaleon_google_analytic_config')
     if camaleon_google_analytics_meta.has_key? :code
       camaleon_google_analytics_code = camaleon_google_analytics_meta[:code]
-      append_pre_asset_content("<script>window.GAID='#{camaleon_google_analytics_code}';</script>")
-      append_asset_libraries({camaleon_google_analytic: {js: [plugin_gem_asset('google_analytics')]}})
+      #append_pre_asset_content("<script>window.GAID='#{camaleon_google_analytics_code}';</script>")
+      #append_asset_libraries({camaleon_google_analytic: {js: [plugin_gem_asset('google_analytics')]}})
+      append_pre_asset_content("<script>
+                                  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                                  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                                  ga('create', '#{camaleon_google_analytics_code}', 'auto');
+                                  ga('send', 'pageview');
+                                </script>")
     end
   end
 
